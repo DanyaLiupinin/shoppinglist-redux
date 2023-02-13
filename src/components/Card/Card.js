55ccdfa4e8f5f20ea1bae2cf1dd7c1e91bf4ec3e
Danya Kovan <mr.kovan_2002@mail.ru>
2023-02-13 09:28:56 +0400

    delete-button added

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,27 +1,33 @@
 import { useState } from 'react'
 import './Card.css'
+import deleteButton from '../../images/delete.png'
 
 function Card(props) {
 
     const [obtained, setObtained] = useState(false)
-    
-    function onBuy () {
+
+    function onBuy() {
         setObtained(!obtained)
     }
 
     return (
-        <div className={`card 
+        <>
+            <div className={`card 
         ${obtained ? 'card_obtained' : ''}
         `}>
-            <label className='card__checkbox-label'>
-                <input className='card__checkbox' type='checkbox' checked={obtained} onChange={onBuy} />
-            </label>
-            <div className='card__info'>
-                <h2 className={`card__product-title 
-                ${obtained ? 'card__product-title_obtained' : '' } `}>{props.title}</h2>
-                <p className='card__product-amount'>{props.amount}</p>
+                <label className='card__checkbox-label'>
+                    <input className='card__checkbox' type='checkbox' checked={obtained} onChange={onBuy} />
+                </label>
+                <div className='card__info'>
+                    <h2 className={`card__product-title 
+                ${obtained ? 'card__product-title_obtained' : ''} `}>{props.title}</h2>
+                    <p className='card__product-amount'>{props.amount}</p>
+                </div>
+                <button className='card__button-delete' type='button'>
+                    <img className='card__delete-icon' src={deleteButton} alt='удалить продукт из списка'></img>
+                </button>
             </div>
-        </div>
+        </>
     )
 }
 
